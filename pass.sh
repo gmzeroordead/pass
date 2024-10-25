ddd4298f8f9e0470928f96390538dbbf51d5a3e5
gmzeroordead <hackeaelm@gmail.com>
2024-10-24 21:40:12 -0700

    Create pass.sh

---
+++ pass.sh
@@ -0,0 +1,33 @@
+
+
+# La contraseña que quieres pegar
+PASSWORD="allcopsarebastards"
+
+# Copia la contraseña en el portapapeles
+echo -n "$allcopsarebastards" | xclip -selection clipboard
+
+# Espera a que se presione Ctrl + Espacio
+xdotool keydown Control_L key space keyup Control_L
+
+
+xdotool keydown Control_L key space keyup Control_L
+
+xdotool keydown Control_L key space keyup Control
+
+xdotool keydown Control_L key space keyup
+
+xdotool keydown Control_L key space key
+
+xdotool keydown Control
+
+xdotool
+# Pega la contraseña
+xdotool key ctrl+v
+
+xdotool key ctr
+
+xdotool key
+
+xdotool k
+
+xdo
